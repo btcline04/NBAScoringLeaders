331276d608913160c2a5144402cd3c03e036734f
btcline04 <btcline04@gmail.com>
2017-01-15 16:12:01 -0500

    adjusted years output in CLI

--- lib/nba_scoring_leaders/cli.rb
+++ lib/nba_scoring_leaders/cli.rb
@@ -2,14 +2,13 @@ class NBAScoringLeaders::CLI
 
   def call
     puts "I'm here to tell you the Yearly NBA Scoring Leaders"
-    puts ""
     list_scoring_leaders
   end
 
   def list_scoring_leaders
     puts ""
-    puts "Which years would you like to see first? 1970-1980, 1981-1990, 1991-2000, 2001-2010, 2011-15?    
-    puts ""
+    puts "Which years would you like to see first? 1985-1995, 1996-2005, 2006-2015?
+
     input = gets.strip.to_i
   end
 
---
+++ lib/nba_scoring_leaders/year.rb
@@ -0,0 +1,10 @@
+class NBAScoringLeaders::Year
+
+
+
+
+
+
+
+
+end
