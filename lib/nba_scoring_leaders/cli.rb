49d1670a2a7324822914ddb448e403c3a47161e6
btcline04 <btcline04@gmail.com>
2017-01-15 15:04:09 -0500

    reworking - backtracking

--- lib/nba_scoring_leaders/cli.rb
+++ lib/nba_scoring_leaders/cli.rb
@@ -1,7 +1,6 @@
 class NBAScoringLeaders::CLI
 
   def call
-    NBAScoringLeaders::Scraper.new.make_nba_scoring_leaders
     puts "I'm here to tell you the Yearly NBA Scoring Leaders"
     puts ""
     list_scoring_leaders
@@ -9,39 +8,9 @@ class NBAScoringLeaders::CLI
 
   def list_scoring_leaders
     puts ""
-    puts "Which years would you like to see first? 1990-2000, 2001-2010, 2011-2015?"
+    puts "Which years would you like to see first? 1970-1980, 1981-1990, 1991-2000, 2001-2010, 2011-15?    
     puts ""
     input = gets.strip.to_i
-
-    print_scoring_list(input)
-
-    puts ""
-    puts "Which year would you like more information on?"
-    input = gets.strip.to_i
-
-    year = NBAScoringLeaders::Year.find(input.to_i)
-
-    print_year(year)
-
-    puts ""
-    puts "Would you like to know more about a different year? Enter Y or N"
-
-    input = gets.strip.downcase
-    if input == "y"
-      call
-    else
-      puts ""
-      puts "No worries - enjoy beating your friends in trivia with your new NBA knowledge!"
-      exit
-    end
   end
-
-  def print_scoring_list(years)
-    puts ""
-    NBAScoringLeaders::Year.all[years-1, 10].each_with_index(years) do |player, year|
-      puts "#{year} - #{year.player} - #{year.scoring_average}"
-    end
-  end
-
 
 end
