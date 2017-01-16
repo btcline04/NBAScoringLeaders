591400898b62e08f70d64a49937574e70385767f
btcline04 <btcline04@gmail.com>
2017-01-16 18:23:30 -0500

    updated CLI w/ code I wish I had

--- lib/nba_scoring_leaders/cli.rb
+++ lib/nba_scoring_leaders/cli.rb
@@ -1,15 +1,38 @@
 class NBAScoringLeaders::CLI
 
-  def call
-    puts "I'm here to tell you the Yearly NBA Scoring Leaders"
+  def start
+    puts "I'm here to tell you the Yearly NBA Scoring Leaders."
     list_scoring_leaders
   end
 
   def list_scoring_leaders
     puts ""
-    puts "Which years would you like to see first? 1985-1995, 1996-2005, 2006-2015?
-
+    puts "Which years would you like to see first? 1985-1995, 1996-2005, 2006-2015?"
     input = gets.strip.to_i
-  end
+
+    nba_years(input)
+    
+    puts ""
+    puts "Type a year to learn more about that scoring champion."
+    input = gets.strip
+
+    year = NBAScoringLeaders::Year.find(input.to_i)
+
+    print_year_with_info(year)
+
+    puts ""
+    puts "Would you like to learn more about a specific year? Enter Y or N."
+    inputs = gets.strip.downcase
+      if input == "y"
+        start
+      else
+        puts ""
+        puts "Cool - your new knowledge of NBA trivia will easily impress all of your friends."
+        exit
+      end
+    end
+
+
+
 
 end
