d736789f1d11636352b431f691496e43444518f3
btcline04 <btcline04@gmail.com>
2017-01-19 20:29:32 -0500

    added scraping paths in year.rb

--- lib/nba_scoring_leaders/year.rb
+++ lib/nba_scoring_leaders/year.rb
@@ -19,6 +19,14 @@ class NbaScoringLeaders::Year
   def self.scrape_lob
     doc = Nokogiri::HTML(open("http://www.landofbasketball.com/awards/nba_scoring_leader_year.htm"))
     binding.pry
+
+    year = doc.xpath("//tr[4]/td[1]").text.gsub(/-.*/, "")
+    player = doc.xpath("//tr[4]/td[2]").text
+    team = doc.xpath("//tr[4]/td[4]").text
+    average = doc.xpath("//tr[4]/td[5]").text.strip
+    total_points = doc.xpath("//tr[4]/td[6]").text.strip
+    games_played = doc.xpath("//tr[4]/td[7]").text.strip
+
   end
 
   # def year
