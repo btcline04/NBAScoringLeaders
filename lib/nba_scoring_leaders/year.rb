23449564c030138b042dfde5d26ca05692cf0041
btcline04 <btcline04@gmail.com>
2017-02-06 21:14:09 -0500

    refactor

--- lib/nba_scoring_leaders/year.rb
+++ lib/nba_scoring_leaders/year.rb
@@ -4,18 +4,6 @@ class NbaScoringLeaders::Year
 
   @@all = []
 
-  # create new Year instances using scraped data & initialized w/ instance variables listed below
-  # def self.new_from_index(tr)
-  #   self.new(
-  #     tr.css("td[1]").text.gsub(/-.*/, ""),
-  #     tr.css("td[2]").text.strip,
-  #     tr.css("td[4]").text.strip,
-  #     tr.css("td[5]").text.strip,
-  #     tr.css("td[6]").text.strip,
-  #     tr.css("td[7]").text.strip,
-  #   )
-  # end
-
   def initialize(year = nil, player = nil, team = nil, average = nil, total_points = nil, games_played = nil)
     @year = year.to_i
     @player = player
